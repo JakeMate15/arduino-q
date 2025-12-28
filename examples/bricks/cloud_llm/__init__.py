7b2bea8698a1d73b5e53af080aa33e8244c98de5
JakeMate15 <erik.mofe51@gmail.com>
2025-12-28 05:37:47 +0000

    Move project from examples

--- examples/bricks/cloud_llm/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
-#
-# SPDX-License-Identifier: MPL-2.0
-
-from .cloud_llm import CloudLLM
-from .models import CloudModel
-
-__all__ = ["CloudLLM", "CloudModel"]
