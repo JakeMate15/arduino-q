59564cad815ff13fe7d087c7c9dbd08761541daf
JakeMate15 <88561069+JakeMate15@users.noreply.github.com>
2025-12-29 12:55:21 -0600

    Sensores arduino

--- ArduinoApps/robot-seguidor-de-pared/sketch/sketch.ino
+++ ArduinoApps/robot-seguidor-de-pared/sketch/sketch.ino
@@ -1,9 +1,45 @@
+// === ULTRASÓNICOS ===
+#define TRIG_DER 10
+#define ECHO_DER 11
+#define TRIG_CENTRO 12
+#define ECHO_CENTRO 13
+const float VEL_SONIDO = 0.035103f;
+
+unsigned long pulseInManual(uint8_t pin, uint8_t state, unsigned long timeout) {
+  unsigned long start = micros();
+  while (digitalRead(pin) == state) { if (micros() - start > timeout) return 0; }
+  while (digitalRead(pin) != state) { if (micros() - start > timeout) return 0; }
+  unsigned long pulseStart = micros();
+  while (digitalRead(pin) == state) { if (micros() - pulseStart > timeout) return 0; }
+  return micros() - pulseStart;
+}
+
+float distanciaCM(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  unsigned long dur = pulseInManual(echo, HIGH, 25000); 
+  if (dur == 0) return -1.0f;
+  return (dur * VEL_SONIDO) / 2.0f;
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Bridge.begin();
+
+  pinMode(TRIG_CENTRO, OUTPUT);
+  pinMode(ECHO_CENTRO, INPUT);
+  pinMode(TRIG_DER, OUTPUT);
+  pinMode(ECHO_DER, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float dC = distanciaCM(TRIG_CENTRO, ECHO_CENTRO);
+  float dR = distanciaCM(TRIG_DER, ECHO_DER);
 
+  Bridge.notify("distancias", dC, dR);
+
+  delay(20);
 }
